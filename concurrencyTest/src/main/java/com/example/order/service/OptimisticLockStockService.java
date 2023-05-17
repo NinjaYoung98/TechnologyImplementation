1f8605b3b952ba03639a56cf1958ee5a6bc5538d
ChoChanYoung <whcksdud8@gmail.com>
2023-05-17 23:52:19 +0900

    #2 Optimistic Lock
    - 낙관적 락을 통해 동시성 문제를 제어해본다

---
+++ concurrencyTest/src/main/java/com/example/order/service/OptimisticLockStockService.java
@@ -0,0 +1,23 @@
+package com.example.order.service;
+
+import com.example.order.domain.entity.Stock;
+import com.example.order.repository.StockRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Getter
+@Service
+public class OptimisticLockStockService {
+    private final StockRepository stockRepository;
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithOptimisticLock(id).orElseThrow();
+        stock.decrease(quantity);
+        stockRepository.saveAndFlush(stock);
+    }
+
+}
